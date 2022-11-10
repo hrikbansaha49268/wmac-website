4c715936e5d330bae2fb5b3806bf9c3c7f810e69
hrikbansaha49268 <73658678+hrikbansaha49268@users.noreply.github.com>
2022-11-10 18:34:19 +0530

    Initial Commit

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  // darkMode: 'media',
+  content: ["./public/pages/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
