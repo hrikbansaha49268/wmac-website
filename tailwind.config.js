c52a907b8dedf6e1473266064c68bd897e6d57ae
hrikbansaha49268 <73658678+hrikbansaha49268@users.noreply.github.com>
2023-09-21 20:45:54 +0530

    SOme font updation

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./public/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'montserrat': ['Montserrat', 'sans-serif']
+      }
+    },
   },
   plugins: [],
 }
