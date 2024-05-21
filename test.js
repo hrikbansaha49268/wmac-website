92b52f64cbde2b9236d9ef74e98b5c428b8249b5
Innovation Web Management <innovation.webmgmt@gmail.com>
2024-05-21 15:15:01 +0530

    Some updates

---
+++ test.js
@@ -0,0 +1,52 @@
+const admList = [
+    // Shihan Dai Koustav Sarkar
+    {
+        colorSchema: '#FFD700',
+        memName: 'Shihan Dai Koustav Sarkar',
+        objPos: '-15px 10%',
+        imgSrc: 'koustav_sarkar.jpeg',
+        scaling: 'scale:2.5',
+        objFit: 'cover',
+        role: `
+        <p class="mt-4 font-semibold">
+            President
+        </p>
+        `
+    },
+    // Sensei Rohan Ganguly
+    {
+        colorSchema: '#a78a7f',
+        memName: 'Sensei Rohan Ganguly',
+        objPos: '20% 30px',
+        imgSrc: 'rohan_ganguly.jpeg',
+        scaling: 'scale:2.5',
+        objFit: 'cover',
+        role: `<p class="mt-4 font-semibold">
+            Vice President
+        </p>`
+    },
+    // Sensei Dipika Biswas
+    {
+        colorSchema: '#645CBB',
+        memName: 'Sensei Dipika Biswas',
+        objPos: '-2px -60%',
+        imgSrc: 'dipika_biswas.jpeg',
+        scaling: 'scale:5.0',
+        objFit: 'cover',
+        role: `<p class="mt-4 font-semibold">
+           Treasurer
+        </p>`
+    },
+    // Sensei Mainak Biswas
+    {
+        colorSchema: '#38302e',
+        memName: 'Sensei Mainak Biswas',
+        objPos: '25% 25%',
+        imgSrc: 'mainak_biswas.JPG',
+        scaling: 'scale:2.2',
+        objFit: 'cover',
+        role: `<p class="mt-4 font-semibold">
+           Secretary General
+        </p>`
+    },
+];
